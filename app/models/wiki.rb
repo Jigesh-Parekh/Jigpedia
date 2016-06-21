6cd8e51a7848ee95d70cb1e5e10987b6a50566a0
Jigesh Parekh <JParekh117@gmail.com>
2016-06-20 22:49:40 -0700

    Sort by User Role Additions

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,21 +1,37 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
-  before_create { self.private = false; true}
+  before_create { self.private = self.private || false; true}
  # scope :visible_to, -> (user) { user.admin? || user. ? all : where(private: false)}
 
-  scope :public_view, -> {where(private: false)}
+  scope :public_view, -> {where(private: true)}
   #scope :private_view, -> {user.admin ? user.premium : where(private = true)} 
-  scope :private_view, -> {where(private: true)} 
-  scope :visible_to, -> (user) {user ? all : public_view; user.premium? || user.admin? ? all : private_view}
+  scope :private_view, -> {where(private: false)} 
+  #scope :visible_to, -> (user) {user.standard? ? public_view : public_view; user.premium? || user.admin? ?  public_view : private_view}
 
  
- #def self.visible_to (user)
- #		if user.admin?
- #			all
- #		elsif user.premium?
- #			where(private: true , private: false)
- #		elsif user.standard?
- #			where(private: false)
- #		end
-  #end
+ def self.visible_to (user)
+ 	     privVar = where(private: true)
+  		 pubVar = where(private: false)	
+ 		if user.admin?
+ 				all
+ 		elsif user.premium? 		
+ 				
+
+ 				
+ 				all 
+
+ 				#if current_user?
+ 				#	privVar && pubVar
+ 				#else
+ 				#	pubVar
+ 				#end
+
+
+ 				#privVar && pubVar
+ 				#where(private: true) #OR private: false )  #Wiki.all 
+ 				#where(private: false)
+ 		elsif user.standard?
+ 				where(private: false)
+ 		end
+  end
 end
